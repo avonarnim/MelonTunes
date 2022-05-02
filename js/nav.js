4bd88cd095c751530055131296ece1a46b74d984
Adam von Arnim <vonarnim@usc.edu>
2022-05-02 01:27:35 -0700

    background, nav

--- js/nav.js
+++ js/nav.js
@@ -2,8 +2,10 @@ jQuery(document).ready(function ($) {
   $(window).scroll(function () {
     if ($(window).scrollTop() >= $(window).height() - $(".navbar").height()) {
       $(".cardinal-back").css("background", "rgb(25, 25, 75)");
+      $(".navbar-dark .navbar-nav .nav-link").css("color", "white");
     } else {
       $(".cardinal-back").css("background", "transparent");
+      $(".navbar-dark .navbar-nav .nav-link").css("color", "#322b8e");
     }
   });
 });
